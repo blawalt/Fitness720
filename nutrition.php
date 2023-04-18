06c6dd414ceee059060ffc4772d50e84e97a9d1d
Beau LaWalt <horace.lawalt.18@cnu.edu>
2023-04-18 11:33:48 -0400

    added nutins.php

--- nutrition.php
+++ nutrition.php
@@ -1,5 +1,11 @@
-<?php
+<html>
 
-echo "hello";
+<form method ="get" action = nutins.php>
+    <label for="date">Select a date:</label>
+    <input type="date" id ="date" name="date">
+    <input type="submit" value="Submit">
 
-?>
+</form>
+
+
+</html>
