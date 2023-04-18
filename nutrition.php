48d1b1fe5e7c996d83c847844055efc0d51cd8c2
Beau LaWalt <horace.lawalt.18@cnu.edu>
2023-04-18 13:28:46 -0400

    found fix for only first word being read in

--- nutrition.php
+++ nutrition.php
@@ -1,10 +1,32 @@
 <html>
 
-<form method ="get" action = nutins.php>
+<form method ="get" >
     <label for="date">Select a date:</label>
     <input type="date" id ="date" name="date">
-    <input type="submit" value="Submit">
+    <br><br><br><br>
+    <select name="recipe" id="recipe">
+        <?php
+        include 'connect.php';
+        //Feel like there needs to be some sort of dict reading in recipeID and associating with recipeId.
+        // SQL query to fetch recipes
+        $sql1 = "SELECT recipeName FROM recipes";
+        $result1 = $conn->query($sql1);
 
+        // Check if the query returned any results
+        if ($result1->num_rows > 0) {
+            // Loop through the results and create an option for each ingredient
+            while($row = $result1->fetch_assoc()) {
+            
+            
+                echo '<option value="'.$row["recipeName"].'">' . $row["recipeName"] .' </option>';
+
+            }
+        } else {
+            echo "<option value=''>No recipes found</option>";
+        }
+        ?>
+    </select>
+    <input type="submit" value="Submit">
 </form>
 
 
@@ -12,9 +34,9 @@
 
 <?php
 
-if (isset($_GET['submit']))
+if (isset($_GET['date']))
 
-    echo "yurd";
+    
 
 
 
